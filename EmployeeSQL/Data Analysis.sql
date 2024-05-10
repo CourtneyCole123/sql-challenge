7ed3684cd632b2779176551d7c046ab8136462b5
CourtneyCole123 <162069113+CourtneyCole123@users.noreply.github.com>
2024-05-10 15:13:30 -0500

    answered up to question 4

--- EmployeeSQL/Data Analysis.sql
+++ EmployeeSQL/Data Analysis.sql
@@ -28,6 +28,12 @@ dept_emp.dept_no = depts.dept_no
 
 --4: List the department number for each employee along with that employee’s employee number,
 --last name, first name, and department name
+SELECT dept_emp.dept_no, employees.emp_no, last_name, first_name, dept_name
+FROM employees
+JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+JOIN depts ON
+dept_emp.dept_no = depts.dept_no 
 
 --5: List first name, last name, and sex of each employee whose first name is
 --Hercules and whose last name begins with the letter B
